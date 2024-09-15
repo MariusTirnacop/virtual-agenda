340c0baa9ef58a1bdb49618e2dfdfe67c03136cf
MariusTirnacop <marius0228@gmail.com>
2024-09-16 00:59:22 +0300

    add app logo and title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
 import { Stack, Typography } from "@mui/material";
+import { AppLogo } from "../assets/AppLogo";
 
 const Header = () => {
   return (
-    <Stack width="100%" alignItems="center" justifyContent="center" p={2}>
+    <Stack
+      width="100%"
+      alignItems="center"
+      justifyContent="center"
+      p={2}
+      gap={2}
+      flexDirection="row"
+    >
       <Typography variant="h4">Virtual Agenda</Typography>
+      <AppLogo width={45} height={45} />
     </Stack>
   );
 };
